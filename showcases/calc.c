c62672a997f13e4bee4d989100b4f5ed33c1739c
ThxImpulse <97831192+ThxImpulse@users.noreply.github.com>
2026-05-20 20:59:15 +0200

    calc

---
+++ showcases/calc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int x = 0;
+    double y = 0.0;
+    double z = 0.0;
+    char choice = '\0';
+
+    printf("Whats your first number: ");
+    scanf("%d", &x);
+
+    printf("Whats your second number: ");
+    scanf("%lf", &y);
+
+    getchar();
+    printf("What would you like to do (+, -, *, /, ^): ");
+    scanf("%c", &choice);
+
+    // we can clear the white space char by either " %c" or getchar();
+    // I lowkey like getchar() cause with fgets() its only getchar()
+    // and I wanna be consistant in my code
+
+    switch (choice) {
+
+        case '+': {
+            z = x + y;
+            printf("Add: %d + %.0lf = %.2lf", x, y, z);
+            break;
+        }
+        case '-': {
+            z = x - y;
+            printf("Sub: %d - %.0lf = %.2lf", x, y, z);
+            break;
+        }
+        case '*': {
+            z = x * y;
+            printf("Mul: %d * %.0lf = %.2lf", x, y, z);
+            break;
+        }
+        case '/': {
+            z = x / y;
+            printf("Dev: %d / %.0lf = %.2lf", x, y, z);
+            break;
+        }
+        case '^': {
+            z = pow(x, y);
+            printf("Pow: %d ^ %.0lf = %.2lf", x, y, z);
+            break;
+        }
+    }
+
+    return 0;
+}
